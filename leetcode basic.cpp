37f165765b795e3d9fe8e87b79232c925feb139d
JAYANT31DADHICH <84202152+JAYANT31DADHICH@users.noreply.github.com>
2023-08-21 14:49:34 +0530

    Update leetcode basic.cpp

--- leetcode basic.cpp
+++ leetcode basic.cpp
@@ -34,16 +34,27 @@ int main() {
         b=newNumber;
     }*/
     // prime number
-    /*int n;
+    /*// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+        // prime number
+    int n;
     cin>>n;
-    for(int i=1;i<=n;i++){
+    int count =0;
+    for(int i=2;i<n;i++){
         if(n%i==0){
-            cout<<i<<":is prime"<<" "<<endl;
-        }
-        else{
+            count=count+1;
             cout<<i<<":not prime"<<" "<<endl;
         }
-    }*/
+        else{
+            cout<<i<<":is prime"<<" "<<endl;
+        }
+    }
+    cout<<count<<endl;
+}*/
 /*    int n;
     cin>>n;
     bool isprime=1;
