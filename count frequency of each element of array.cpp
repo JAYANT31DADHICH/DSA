76a8d19aff2af94d5387a809950028e8f2c7a0a2
JAYANT31DADHICH <84202152+JAYANT31DADHICH@users.noreply.github.com>
2024-05-03 23:31:26 +0530

    Create count frequency of each element of array.cpp

---
+++ count frequency of each element of array.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include <climits>
+using namespace std;
+void countFreq(vector<int>& arr1){
+    int max=INT_MIN;
+    for(int i=0;i<arr1.size();i++){
+        if(arr1[i]>max){
+            max=arr1[i];
+        }
+    }
+    vector<int> arr2(max+1,0);
+    for(int i=0;i<arr1.size();i++){
+        arr2[arr1[i]]++;
+    }
+    cout<<"count frequency of each element:"<<endl;
+    for(int i=0;i<arr1.size();i++){
+        if(arr2[arr1[i]]>0) {
+            cout<<arr2[arr1[i]]<<endl;
+            arr2[arr1[i]]=-1;  // changing arr val to -1
+        }
+        
+    }
+    
+}
+int main(){
+    cout<<"enter the length:"<<endl;
+    int length;
+    cin>>length;
+    cout<<"enter the elements"<<endl;
+    vector<int> arr(length);
+    for(int i=0;i<length;i++){
+        cin>>arr[i];
+    }
+    //countFreq(arr);
+    
+}
